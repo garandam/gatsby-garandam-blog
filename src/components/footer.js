7571ecbc61abc006e3bbb8ccef6671b2650a784a
Grischa Aranda Muñoz <grischa.aranda@core-it.ch>
2020-01-05 11:12:31 +0100

    feat: initial footer styling with tailwindcss

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,16 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <footer>
-      © {new Date().getFullYear()}, Created by Grischa Aranda Muñoz with
-      {` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    <footer class="bg-white border-t border-gray-400">
+      <div class="container flex max-w-4xl mx-auto py-8">
+        <div class="text-gray-600 text-sm text-center w-full py-4">
+          © {new Date().getFullYear()}, Created by Grischa Aranda Muñoz with
+          {` `}
+          <a class="underline" href="https://www.gatsbyjs.org">
+            Gatsby
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
