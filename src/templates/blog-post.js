e618fcc265185373db23280df7e9471a64934e64
Grischa Aranda Muñoz <grischa.aranda@core-it.ch>
2020-01-07 19:41:45 +0100

    feat: style blog-post with tailwindcss

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -17,28 +17,39 @@ class BlogPostTemplate extends React.Component {
           title={post.frontmatter.title}
           description={post.frontmatter.description || post.excerpt}
         />
-        <div className="markdown">
-          <h1>{post.frontmatter.title}</h1>
-          <p>{post.frontmatter.date}</p>
-          <MDXRenderer>{post.body}</MDXRenderer>
-          <hr />
 
-          <ul>
-            <li>
-              {previous && (
-                <Link to={`/blog${previous.fields.slug}`} rel="prev">
-                  ← {previous.frontmatter.title}
-                </Link>
-              )}
-            </li>
-            <li>
-              {next && (
-                <Link to={`/blog${next.fields.slug}`} rel="next">
-                  {next.frontmatter.title} →
-                </Link>
-              )}
-            </li>
-          </ul>
+        <div>
+          <Link to="/blog/" className="text-base text-teal-500 font-bold">
+            ← Back to Blog
+          </Link>
+          <h1 className="text-3xl md:text-4xl font-bold text-black pt-6 pb-2 break-normal">
+            {post.frontmatter.title}
+          </h1>
+          <p className="text-sm md:text-base font-normal text-gray-500">
+            {post.frontmatter.date}
+          </p>
+        </div>
+
+        <div className="markdown">
+          <MDXRenderer>{post.body}</MDXRenderer>
+        </div>
+        <hr className="border-b-2 border-gray-300 mb-8 mx-4" />
+
+        <div className="flex justify-between px-4 pb-12 text-teal-500 font-bold text-sm">
+          <div>
+            {previous && (
+              <Link to={`/blog${previous.fields.slug}`} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </div>
+          <div>
+            {next && (
+              <Link to={`/blog${next.fields.slug}`} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </div>
         </div>
       </Layout>
     )
